1137de26748b27dc85374792e28ec3313d95373d
bitbasher <vulcan_@mac.com>
2025-12-02 10:30:18 +0100

    added debug statements to main to be sure it was running

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -4,17 +4,23 @@
  */
 
 #include <QApplication>
+#include <QDebug>
 #include "mainwindow.h"
 
 int main(int argc, char *argv[]) {
+    qDebug() << "Starting CppSnippets application...";
+    
     QApplication app(argc, argv);
     
     app.setApplicationName("CppSnippets");
     app.setApplicationVersion("1.0.0");
     app.setOrganizationName("CppSnippets");
     
+    qDebug() << "Creating main window...";
     MainWindow window;
+    qDebug() << "Showing main window...";
     window.show();
+    qDebug() << "Entering event loop...";
     
     return app.exec();
 }
